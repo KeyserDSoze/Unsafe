20edb5f13a4d178fa5be1578fab2bd08293036ee
Alessandro Rapiti <a.rapiti@vetrya.com>
2018-10-24 22:45:55 +0200

    Add project files.

---
+++ Unsafe.App/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Unsafe.Base;
+
+namespace Unsafe.App
+{
+    class Program
+    {
+        unsafe static void Main(string[] args)
+        {
+            int sample = 5;
+            Example.Test(&sample);
+            Console.WriteLine(sample);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Unsafe.Base/Example.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Unsafe.Base
+{
+    public class Example
+    {
+        //public unsafe static void Test(SubClass* pointer)
+        public unsafe static void Test(int* pointer)
+        {
+            SubClass subClass = new SubClass() { Hello = "Hi" };
+            string value = "Hello";
+            #region cannot take the address of, get the size of, or declare a pointer to a managed type
+            //int* pointer = &subClass;
+            //int* pointer = &subClass.Hello;
+            //int* pointer = &value;
+            #endregion
+            *pointer *= *pointer;
+            Console.WriteLine(*pointer);
+            //Console.WriteLine(pointer);
+        }
+        public class SubClass
+        {
+            public string Hello { get; set; }
+        }
+    }
+}
